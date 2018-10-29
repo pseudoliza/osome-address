401b1efceb4df32917f52aad61be0981db7580a0
AlekseyLeshko <alekseyleshko@gmail.com>
2018-10-30 00:38:50 +0300

    Implement osome address

--- scr/index.js
+++ scr/index.js
@@ -1,3 +1,16 @@
-const address = 'osome address'
+const houseNumber = '80'
+const streetName = 'Robinson Road'
+const floor = '08'
+const apartment = '01'
+const country = 'Singapore'
+const zipCode = '068898'
 
-module.exports = address
+module.exports = {
+  houseNumber,
+  streetName,
+  floor,
+  apartment,
+  country,
+  zipCode,
+  fullAddress: `${houseNumber} ${streetName}, #${floor}-${apartment}, ${country} ${zipCode}`,
+}
